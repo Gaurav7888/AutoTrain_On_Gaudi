646cdf62bf74fd2fd887adcd9b3dc072f4b6537a
Vikhyathraj Shetty K <itsvikhyathraj@gmail.com>
2024-09-03 19:21:10 +0530

    Fix: Tasks Config not in separate config file

---
+++ ui/config/tasksConfig.jsx
@@ -0,0 +1,46 @@
+export const tasksOpts = [
+  {
+    category: "LLM",
+    color: "#023e8a",
+    options: [
+      {
+        value: "llm:sft",
+        label: "Supervised Fine Tuning",
+        borderColor: "#0077b6",
+      },
+    ],
+  },
+  {
+    category: "Text",
+    color: "#015b9f",
+    options: [
+      {
+        value: "text-classification",
+        label: "Text Classification",
+        borderColor: "#00b4d8",
+      },
+    ],
+  },
+  {
+    category: "Audio",
+    color: "#0077b6",
+    options: [
+      {
+        value: "audio-classification",
+        label: "Audio Classification",
+        borderColor: "#48cae4",
+      },
+    ],
+  },
+  {
+    category: "Image",
+    color: "#0096c7",
+    options: [
+      {
+        value: "image-generation",
+        label: "Image Generation",
+        borderColor: "#ade8f4",
+      },
+    ],
+  },
+];
